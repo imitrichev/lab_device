ffead91f00fd9858ba0db8845223f902192b0605
kit1ket <99211435+kit1ket@users.noreply.github.com>
2023-09-06 12:34:15 +0300

    Update device.cpp
    Добавление комментариев

--- device.cpp
+++ device.cpp
@@ -36,10 +36,14 @@ class Drobilka : public Device
 {
 public:
     Drobilka() {}  // Конструктор
+// Обновление выходных потоков и проверка условия одного входа и выхода
     void updateOutputs() override {
         if (inputs.size() == 1 && outputs.size() == 1) {
+            // Получение массового расхода входного потока
             double inputMassFlow = inputs[0]->getMassFlow();
-            double outputMassFlow = inputMassFlow * 3;
+            // Получение выходного потока согласно массовому расходу
+            double outputMassFlow = inputMassFlow * 0.3;
+            // Устанавление массового расхода для выходного потока
             outputs[0]->setMassFlow(outputMassFlow);
         }
     }
@@ -49,14 +53,18 @@ int main()
 {
     streamcounter=0;
     //Mixer d1;
+    // Создание экземпляра класса
     Drobilka drobilka;
     shared_ptr<Stream> s1(new Stream(++streamcounter));
     shared_ptr<Stream> s2(new Stream(++streamcounter));
 
     s1->setMassFlow(10.0);
+    // Добавление потоков в дробилку
     drobilka.addInput(s1);
     drobilka.addOutput(s2);
+    // Обновление выходного потока
     drobilka.updateOutputs();
+    // Вывод входного и выходного потока
     s1->print();
     s2->print();
    
