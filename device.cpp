a897e8a7e2f8e4c3bfcdce11e9f7b97b00855b69
IlyaFiskhanove <ilya.honda-2000@yandex.ru>
2023-09-06 13:45:06 +0300

    Тесты

--- device.cpp
+++ device.cpp
@@ -1,82 +1,95 @@
 #include <iostream>
-#include <string>
-#include <vector>
+#include <cassert>
 #include <memory>
+#include <vector>
+#include <string>
 
 using namespace std;
 int streamcounter;
 
 class Stream
 {
-    private:
-      double mass_flow;
-      string name;
-    public:
-      Stream(int s){setName("s"+std::to_string(s));}
-      void setName(string s){name=s;}
-      string getName(){return name;}
-      void setMassFlow(double m){mass_flow=m;}
-      double getMassFlow(){return mass_flow;}
-      void print(){cout<<"Stream "<<getName()<<" flow = "<<getMassFlow()<<endl;}
+private:
+    double mass_flow;
+    string name;
+
+public:
+    Stream(int s) { setName("s" + std::to_string(s)); }
+    void setName(string s) { name = s; }
+    string getName() { return name; }
+    void setMassFlow(double m) { mass_flow = m; }
+    double getMassFlow() { return mass_flow; }
+    void print() { cout << "Stream " << getName() << " flow = " << getMassFlow() << endl; }
 };
 
 class Device
 {
-    protected:
-      vector<shared_ptr<Stream>> inputs;
-      vector<shared_ptr<Stream>> outputs;
-    public:
-      void addInput(shared_ptr<Stream> s){inputs.push_back(s);}
-      void addOutput(shared_ptr<Stream> s){outputs.push_back(s);}
-      virtual void updateOutputs() = 0;
+protected:
+    vector<shared_ptr<Stream>> inputs;
+    vector<shared_ptr<Stream>> outputs;
+
+public:
+    void addInput(shared_ptr<Stream> s) { inputs.push_back(s); }
+    void addOutput(shared_ptr<Stream> s) { outputs.push_back(s); }
+    virtual void updateOutputs() = 0;
 };
 
 class SimpleColumn : public Device
 {
 public:
-    SimpleColumn() {}  // Конструктор
+    SimpleColumn() {}
 
-    // Метод для обновления выходных потоков на основе входного потока
     void updateOutputs() override {
         if (inputs.size() == 1 && outputs.size() == 2) {
-            // Получаем массовый расход входного потока
             double inputMassFlow = inputs[0]->getMassFlow();
-
-            // Простой пример: делим входной поток поровну на два выходных потока
             double outputMassFlow = inputMassFlow / 2.0;
-
-            // Устанавливаем массовый расход для обоих выходных потоков
             outputs[0]->setMassFlow(outputMassFlow);
             outputs[1]->setMassFlow(outputMassFlow);
         }
     }
 };
 
+void testStream() {
+    // Тест случая для класса Stream
+    shared_ptr<Stream> s(new Stream(1));
+    s->setMassFlow(15.0);
+    
+    // Проверяем, что массовый расход устанавливается и возвращается корректно
+    assert(s->getMassFlow() == 15.0);
+    
+    // Проверяем, что имя объекта устанавливается корректно
+    assert(s->getName() == "s1");
+}
 
-int main()
-{
-    streamcounter = 0;
+void testSimpleColumn() {
+    // Тест случая для класса SimpleColumn
+    shared_ptr<Stream> s1(new Stream(1));
+    shared_ptr<Stream> s2(new Stream(2));
+    shared_ptr<Stream> s3(new Stream(3));
 
-    shared_ptr<Stream> s1(new Stream(++streamcounter));
-    shared_ptr<Stream> s2(new Stream(++streamcounter));
-    shared_ptr<Stream> s3(new Stream(++streamcounter));
     s1->setMassFlow(10.0);
     s2->setMassFlow(5.0);
 
     SimpleColumn column;
-
-    // Добавляем потоки в колонну
     column.addInput(s1);
     column.addOutput(s2);
     column.addOutput(s3);
 
-    // Обновляем выходные потоки
+    // Вызываем метод updateOutputs для SimpleColumn
     column.updateOutputs();
 
-    // Выводим информацию о потоках
-    s1->print();
-    s2->print();
-    s3->print();
+    // Проверяем, что массовый расход установлен корректно для выходных потоков s2 и s3
+    assert(s2->getMassFlow() == 7.5);
+    assert(s3->getMassFlow() == 7.5);
+}
 
+void runTests() {
+    testStream();
+    testSimpleColumn();
+    cout << "All tests passed!" << endl;
+}
+
+int main() {
+    runTests();
     return 0;
 }
