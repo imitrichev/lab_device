fec060620ae788b3a6e08ba92fc4b517b066ee0c
hyfoxus <71084917+hyfoxus@users.noreply.github.com>
2023-09-06 13:19:12 +0300

    Update device.cpp

--- device.cpp
+++ device.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <memory>
 
+
 using namespace std;
 int streamcounter;
 
@@ -29,12 +30,12 @@ class Device
       int outputAmount;
     public:
       void addInput(shared_ptr<Stream> s){
-          if(inputs.size < inputAmount) inputs.push_back(s);
-          else cout << "INPUT STREAM LIMIT!" << endl;
+          if(inputs.size() < inputAmount) inputs.push_back(s);
+          else throw"INPUT STREAM LIMIT!";
       }
       void addOutput(shared_ptr<Stream> s){
-          if(outputs.size < outputAmount) outputs.push_back(s);
-          else cout << "OUTPUT STREAM LIMIT!" << endl;
+          if(outputs.size() < outputAmount) outputs.push_back(s);
+          else throw "OUTPUT STREAM LIMIT!";
       }
       virtual void updateOutputs() = 0;
 };
@@ -45,28 +46,92 @@ class Reactor : Device{
         if (isDoubleReactor) outputAmount = 2;
         else inputAmount = 1;
     }
-    void updateOutputs override(){
-    double inputMass = inputs.at(0) -> getMassFlow();
-        for(int i = 0; i < outputAmount; i++){
-            double outputLocal = inputMass * 0.83;
+    
+    void updateOutputs() override{
+        double inputMass = inputs.at(0) -> getMassFlow();
+            for(int i = 0; i < outputAmount; i++){
+            double outputLocal = inputMass * (1/(outputAmount - i));
             outputs.at(i) -> setMassFlow(outputLocal);
             inputMass -= outputLocal;
         }
     }
-}
+};
 
-//class Mixer.......
-
-int main()
-{
+void testTooManyOutputStreams(){
     streamcounter=0;
-    //Mixer d1;
+    
+    Reactor dl = new Reactor(false);
     
     shared_ptr<Stream> s1(new Stream(++streamcounter));
     shared_ptr<Stream> s2(new Stream(++streamcounter));
     shared_ptr<Stream> s3(new Stream(++streamcounter));
     s1->setMassFlow(10.0);
     s2->setMassFlow(5.0);
+    dl.addInput(s1);
+    dl.addOutput(s2);
+    try{
+        dl.addOutput(s3);
+    } catch(const string ex){
+         if (ex == "OUTPUT STREAM LIMIT!") {
+        cout << "Test 1 passed" << endl;
+
+        return;
+    }
     
-    //d1.addInput......
+     cout << "Test 1 failed" << endl;
 }
+
+void testTooManyInputStreams(){
+    streamcounter=0;
+    
+    Reactor dl = new Reactor(false);
+    
+    shared_ptr<Stream> s1(new Stream(++streamcounter));
+    shared_ptr<Stream> s3(new Stream(++streamcounter));
+    s1->setMassFlow(10.0);
+    s2->setMassFlow(5.0);
+    dl.addInput(s1);
+    try{
+        dl.addInput(s3);
+    } catch(const string ex){
+         if (ex == "INPUT STREAM LIMIT!") {
+        cout << "Test 2 passed" << endl;
+
+        return;
+    }
+    
+     cout << "Test 2 failed"s << endl;
+}
+
+void testInputEqualOutput(){
+        streamcounter=0;
+    
+    Reactor dl = new Reactor(true);
+    
+    shared_ptr<Stream> s1(new Stream(++streamcounter));
+    shared_ptr<Stream> s2(new Stream(++streamcounter));
+    shared_ptr<Stream> s3(new Stream(++streamcounter));
+    s1->setMassFlow(10.0);
+    s2->setMassFlow(5.0);
+    dl.addInput(s1);
+    dl.addOutput(s2);
+    dl.addOutput(s3);
+    
+    dl.updateOutputs();
+    
+    if(dl.outputs.at(0).getMassFlow + dl.outputs.at(1).getMassFlow == dl.inputs.at(0).getMassFlow){
+        cout << "Test 3 passed" << endl;
+    } else cout << "Test 3 failed" << endl;
+}
+
+void tests(){
+    testInputEqualOutput();
+    testTooManyOutputStreams();
+    testTooManyInputStreams();
+}
+
+int main()
+{
+    tests;
+}
+
