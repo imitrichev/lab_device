ed398a4fe2aa0e31c43ffb055b2f2237cd5fb037
Ko1baser <astarikov541@gmail.com>
2023-09-06 14:24:10 +0300

    Added 3 tests

--- device.cpp
+++ device.cpp
@@ -79,6 +79,65 @@ public:
   }
 };
 
+void runTests()
+{
+  streamcounter = 0;
+
+  // Тест 1: Проверка базового функционала ComplexSeparator с 3 выходными потоками
+  ComplexSeparator separator(3);
+
+  shared_ptr<Stream> s1(new Stream(++streamcounter));
+  s1->setMassFlow(10.0);
+  s1->print();
+  separator.addInput(s1);
+
+  shared_ptr<Stream> s2(new Stream(++streamcounter));
+  shared_ptr<Stream> s3(new Stream(++streamcounter));
+  shared_ptr<Stream> s4(new Stream(++streamcounter));
+
+  separator.addOutput(s2);
+  separator.addOutput(s3);
+  separator.addOutput(s4);
+
+  separator.updateOutputs();
+  s2->print();
+  s3->print();
+  s4->print();
+
+  // Тест 2: Проверка ComplexSeparator без входного потока
+  ComplexSeparator separatorNoInput(2);
+  shared_ptr<Stream> s5(new Stream(++streamcounter));
+  separatorNoInput.addOutput(s5);
+
+  try
+  {
+    separatorNoInput.updateOutputs();
+  }
+  catch (const char *error)
+  {
+    cout << "Test 2: " << error << endl;
+  }
+
+  // Тест 3: Проверка ComplexSeparator с недопустимым количеством выходных потоков
+  ComplexSeparator separatorTooManyOutputs(2);
+  shared_ptr<Stream> s6(new Stream(++streamcounter));
+  separatorTooManyOutputs.addInput(s6);
+
+  shared_ptr<Stream> s7(new Stream(++streamcounter));
+  shared_ptr<Stream> s8(new Stream(++streamcounter));
+  separatorTooManyOutputs.addOutput(s7);
+  separatorTooManyOutputs.addOutput(s8);
+
+  try
+  {
+    separatorTooManyOutputs.updateOutputs();
+  }
+  catch (const char *error)
+  {
+    cout << "Test 3: " << error << endl;
+  }
+}
+
 int main()
 {
   streamcounter = 0;
@@ -104,5 +163,6 @@ int main()
   s3->print();
   s4->print();
 
+  runTests();
   return 0;
 }
