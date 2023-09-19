411e91c0368cd6f840d0f2e8f7aa51bcbfd8e112
kit1ket <99211435+kit1ket@users.noreply.github.com>
2023-09-19 21:42:25 +0300

    Update device.cpp add 3 tests

--- device.cpp
+++ device.cpp
@@ -7,6 +7,10 @@
 
 using namespace std;
 int streamcounter;
+int inp_counts = 1;
+const float POSSIBLE_ERROR = 0.01;
+const int DROBILKA_OUTPUTS = 1;
+
 
 class Stream
 {
@@ -24,6 +28,7 @@ class Stream
 
 class Device
 {
+    
     protected:
       vector<shared_ptr<Stream>> inputs;
       vector<shared_ptr<Stream>> outputs;
@@ -36,8 +41,22 @@ class Device
 //class Mixer.......
 class Drobilka : public Device
 {
+    
 public:
     Drobilka() {}  // Конструктор
+    
+    void addInput(shared_ptr<Stream> s) {
+        if (inputs.size() == inp_counts) {
+          throw "Too much inputs";
+        }
+        inputs.push_back(s);
+     }
+    void addOutput(shared_ptr<Stream> s) {
+        if (outputs.size() == DROBILKA_OUTPUTS) {
+          throw "Too much outputs";
+        }
+        outputs.push_back(s);
+    }
 // Обновление выходных потоков и проверка условия одного входа и выхода
     void updateOutputs() override {
         if (inputs.size() == 1 && outputs.size() == 1) {
@@ -53,32 +72,86 @@ public:
         }
     }
 };
-void test()
-    {
-     streamcounter=0;
-    //Mixer d1;
-    // Создание экземпляра класса
+void correctGetMassFlow() {
+    streamcounter=0;
     Drobilka drobilka;
+    
     shared_ptr<Stream> s1(new Stream(++streamcounter));
     shared_ptr<Stream> s2(new Stream(++streamcounter));
-
+    shared_ptr<Stream> s3(new Stream(++streamcounter));
+    
     s1->setMassFlow(10.0);
-    // Добавление потоков в дробилку
+
     drobilka.addInput(s1);
     drobilka.addOutput(s2);
-    // Обновление выходного потока
-    drobilka.updateOutputs();
-    assert(s1->getMassFlow()==10.0);
-    assert(s2->getMassFlow()==10.0);
-    // Вывод входного и выходного потока
-    s1->print();
-    s2->print();
 
-    //d1.addInput......
-        
-    };
+    drobilka.updateOutputs();
+
+    if (abs(s2->getMassFlow()) - 10 < POSSIBLE_ERROR) {
+      cout << "Test 1 passed" << endl;
+    } else {
+      cout << "Test 1 failed" << endl;
+    }
+}
+void correctOutputs() {
+    streamcounter=0;
+    Drobilka drobilka;
+    
+    shared_ptr<Stream> s1(new Stream(++streamcounter));
+    shared_ptr<Stream> s2(new Stream(++streamcounter));
+    shared_ptr<Stream> s3(new Stream(++streamcounter));
+    
+    s1->setMassFlow(10.0);
+    
+    drobilka.addInput(s1);
+    drobilka.addOutput(s2);
+
+    try {
+      drobilka.addOutput(s3);
+    } catch (const char* ex) {
+      if (ex == "Too much outputs") {
+        cout << "Test 2 passed" << endl;
+
+        return;
+      }
+    }
+
+    cout << "Test 2 failed" << endl;
+}
+void correctInputs() {
+    streamcounter=0;
+    Drobilka drobilka;
+    
+    shared_ptr<Stream> s1(new Stream(++streamcounter));
+    shared_ptr<Stream> s2(new Stream(++streamcounter));
+    shared_ptr<Stream> s3(new Stream(++streamcounter));
+
+    s1->setMassFlow(10.0);
+
+    drobilka.addInput(s1);
+    drobilka.addOutput(s2);
+
+    try {
+      drobilka.addInput(s3);
+    } catch (const char* ex) {
+      if (ex == "Too much inputs") {
+        cout << "Test 3 passed" << endl;
+
+        return;
+      }
+    }
+
+    cout << "Test 3 failed" << endl;
+}
+
+void tests() {
+  correctGetMassFlow();
+  correctOutputs();
+  correctInputs();
+}
+
 int main()
 {
-    test();
+    tests();
    
 }
